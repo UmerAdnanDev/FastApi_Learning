84c4f422ea1fff6571ac258354c436a61d7a5f2f
Muhammad Umer Adnan <omeradnannovember2005@gmail.com>
2026-03-14 23:28:20 +0500

    updated model.py to get date from database
    There was a technical issue was working well as no CRUD is intergrated with pgsql . This allows schema access

--- 4-SQLModel/src/products/models.py
+++ 4-SQLModel/src/products/models.py
@@ -7,13 +7,14 @@ class Product(SQLModel,table = True):
   uid: uuid.UUID = Field(
     sa_column=Column(
       pg.UUID,nullable=False,
-      primary_key=True,default=uuid.uuid4()
+      primary_key=True,default=uuid.uuid4
     )
 
   )
   name: str
   price: float
-  created_at:datetime = Field(Column(pg.TIMESTAMP,default=datetime.now))
-  updated_at:datetime =  Field(Column(pg.TIMESTAMP,default=datetime.now))
+  publish_date: datetime
+  created_at:datetime = Field(sa_column=Column(pg.TIMESTAMP,default=datetime.now))
+  updated_at:datetime =  Field(sa_column=Column(pg.TIMESTAMP,default=datetime.now))
   def __repr__(self):
     return f"<product {self.name}>"
