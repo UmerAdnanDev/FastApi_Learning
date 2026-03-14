bed307c52b1e1d20cd4e7875466d0c24b4a5b01b
Muhammad Umer Adnan <omeradnannovember2005@gmail.com>
2026-03-14 23:32:29 +0500

    added create class  for DB operations
    created new pydantic class for creating products with pgsql and added a new attribute publish_date

--- 4-SQLModel/src/products/schemas.py
+++ 4-SQLModel/src/products/schemas.py
@@ -1,8 +1,17 @@
 from pydantic import BaseModel 
+import uuid
+from datetime import datetime
 class Product(BaseModel):
-  id: int
+  uuid: uuid.UUID
   name: str
   price: float
+  publish_date: str
+  created_at: datetime
+  updated_at: datetime
+class ProductCreateModel(BaseModel):
+  name: str
+  price: float
+  publish_date: str
 class ProductUpdateModel(BaseModel):
   name: str
   price: float
