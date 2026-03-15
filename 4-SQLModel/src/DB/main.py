4c5cbd9683cb460ac8124f356360c49b71b03886
Muhammad Umer Adnan <omeradnannovember2005@gmail.com>
2026-03-15 07:14:13 +0500

    added async session method for DB
    it allows session handling for database integration for future dependency injection

--- 4-SQLModel/src/DB/main.py
+++ 4-SQLModel/src/DB/main.py
@@ -1,6 +1,8 @@
 from sqlmodel import create_engine,text,SQLModel
 from sqlalchemy.ext.asyncio import AsyncEngine
 from src.config import Config
+from sqlmodel.ext.asyncio.session import AsyncSession
+from sqlalchemy.orm import sessionmaker
 engine = AsyncEngine(
    create_engine(
    url=Config.DATABASE_URL,echo=True # type: ignore
@@ -9,3 +11,10 @@ async def init_db():
    async with engine.begin() as conn:
      from src.products.models import Product
      await conn.run_sync(SQLModel.metadata.create_all) 
+async def get_session()-> AsyncSession: # type: ignore
+   Session = sessionmaker(
+      bind = engine,class_=AsyncSession, # type: ignore
+      expire_on_commit=False # allows to use session object even after commiting db transactions
+   ) # type: ignore
+   async with Session() as session: # type: ignore
+      yield session # type: ignore
